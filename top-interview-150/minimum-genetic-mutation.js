2b35c16594883c58062f05fe3e83522f5533023b
Rohit <saluja730@gmail.com>
2024-01-31 15:38:46 +0530

    updated the ltter combination is done

--- top-interview-150/minimum-genetic-mutation.js
+++ top-interview-150/minimum-genetic-mutation.js
@@ -5,21 +5,23 @@
  * @return {number}
  */
 var minMutation = function(startGene, endGene, bank) {
-    const choices = ['A','C','G','T'];
+    bank = new Set(bank)
+    if(!bank.has(endGene)) return -1
     const queue = [startGene]
-    const seen = new Set([startGene]);
     let steps = 0
+    const choices = ['A','C','G','T']
+    const seen = new Set([startGene]);
     while(queue.length){
-        const queueLength = queue.length;
+        const queueLength = queue.length
         for(let i=0; i<queueLength; i++){
             const node = queue.shift()
             if(node === endGene) return steps
             for(const choice of choices){
                 for(let j=0; j<node.length; j++){
-                    const neighbor = node.substring(0,j) + choice + node.substring(j+1)
-                    if(!seen.has(neighbor) && bank.includes(neighbor)){
-                        queue.push(neighbor)
-                        seen.add(neighbor)
+                    const next  = node.substring(0,j) + choice + node.substring(j+1)
+                    if(!seen.has(next) && bank.has(next) ){
+                        seen.add(next)
+                        queue.push(next)
                     }
                 }
             }
@@ -27,4 +29,5 @@ var minMutation = function(startGene, endGene, bank) {
         steps = steps + 1
     }
     return -1
-};
+
+}
