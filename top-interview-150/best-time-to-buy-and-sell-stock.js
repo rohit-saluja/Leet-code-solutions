ab8ef427e6568ee68157704c8fa57766d2a1f29c
Rohit <saluja730@gmail.com>
2024-02-08 19:29:32 +0530

    updated the questions list of the orders is done;

--- top-interview-150/best-time-to-buy-and-sell-stock.js
+++ top-interview-150/best-time-to-buy-and-sell-stock.js
@@ -3,18 +3,11 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let left = 0
-    let right = 0
-    let max_profit = 0
-    
-    while (right < prices.length){
-        if(prices[left] < prices[right]){
-            profit = prices[right]-prices[left]
-            max_profit = Math.max(max_profit,profit)
-        }else{
-            left = right
-        }
-        right = right + 1
-    }
-    return max_profit
+    let buy = Infinity
+    let sell = 0
+    for(let i=0; i<prices.length; i++){
+        buy = Math.min(prices[i],buy)
+        sell = Math.max(prices[i]-buy,sell)
+    }   
+    return sell
 };
