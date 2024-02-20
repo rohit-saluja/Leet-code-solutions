c83f6af197481b38e139ca17ad96221a70341669
Rohit <saluja730@gmail.com>
2024-02-20 14:59:42 +0530

    updated the quession

---
+++ top-interview-150/3sum.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    nums.sort((a,b)=>a-b)
+    const result = []
+    for(let i=0; i<nums.length;i++){
+        let left = i+1;
+        let right = nums.length-1
+        while(left < right){
+            let sum = nums[i] + nums[left] + nums[right]
+            if(sum === 0){
+                result.push([nums[i],nums[left],nums[right]])
+                while(nums[left] == nums[left + 1])left = left + 1
+                while(nums[right] === nums[right + 1])right = right - 1
+                left = left + 1
+                right = right - 1
+            }else if (sum < 0)  left = left + 1
+            else right = right - 1
+        }
+        while(nums[i+1] === nums[i]){
+            i=i+1
+        }
+    }
+    return result
+};
